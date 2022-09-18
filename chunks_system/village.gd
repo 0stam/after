aa9feb8b06765d03d1072e1c0252617e58dd0c14
ktostam0 <romanskiba0@gmail.com>
2022-09-18 20:11:31 +0200

    Code cleanup

--- chunks_system/village.gd
+++ chunks_system/village.gd
@@ -5,8 +5,6 @@ extends Area3D
 
 func _on_village_body_entered(body: Node3D) -> void:
 	# TODO: add countdown UI
-	print("Soon")
 	await get_tree().create_timer(randi_range(2, 3))
 	for path in artillery_targets:
 		get_node(path).start()
-		print("Didn't crash")
--- effects/artillery_particles/artillery_particles.gd
+++ effects/artillery_particles/artillery_particles.gd
@@ -7,23 +7,19 @@ const DAMAGE = 100.0
 var speed: float = 10
 var hit_range: float = 30
 
-var despawn_queued: bool = false
-
 var target: Vector3  # Should be set when spawned
 
 @onready var player: Player = References.player
 
 
 func _process(delta: float) -> void:
-	if despawn_queued:
-		return
-	
 	global_transform.origin.y -= speed * delta
 	
-	if global_transform.origin.distance_to(target) < 0.5 and not despawn_queued:
+	if global_transform.origin.distance_to(target) < 0.5:  # When the target is reached
 		set_process(false)
-		despawn_queued = true
 		
+		# Damage equals 100% if player is in 50% of the hit range
+		# Than it scales: 100%-0% damage ~ 50%-100% range
 		var distance: float = global_transform.origin.distance_to(player.global_transform.origin)
 		distance = (distance - (hit_range / 2)) / (hit_range / 2)
 		distance = 1 - clampf(distance, 0, 1)
